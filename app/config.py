e8687048a52da962e1d1fe8ed2d84039b2ba0130
rossyvee <roseakinyi001@gmail.com>
2022-05-06 23:08:58 +0300

    first commit

---
+++ app/config.py
@@ -0,0 +1,11 @@
+class Config:
+    pass
+
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+    
+    DEBUG = True
